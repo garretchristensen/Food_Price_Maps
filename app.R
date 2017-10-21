f73c06029d107ba78e481420bda0325ab50f8bb5
Garret Christensen <garret@berkeley.edu>
2017-10-20 23:58:19 -0700

    xlab on histo

--- app.R
+++ app.R
@@ -5,12 +5,14 @@ ui<-fluidPage(
   sliderInput(inputId = "year",
               label = "Choose a Year",
               value = 2005, min = 1999, max = 2010),
-  plotOutput("yr")
+  plotOutput("yr"),
+  plotOutput("map")
 )
 server<- function(input, output) {
   output$yr <-  renderPlot({
     title <- "Histogram of County TFPs"
-    hist(unlist(df[paste0("TFPYRM", input$year)]), main = title)
+    hist(unlist(df[paste0("TFPYRM", input$year)]), main = title, 
+         xlab = "Dollar Price of Weekly TFP, Family of 4")
   })
 }
 shinyApp(ui = ui, server = server)
