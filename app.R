db54e1be7798f9d8dce5c2af389623e10ec41046
Garret Christensen <garret@berkeley.edu>
2017-10-21 20:21:16 -0700

    working version w state, hist, map

--- app.R
+++ app.R
@@ -1,6 +1,11 @@
 library(shiny)
 library(choroplethr)
 library(choroplethrMaps)
+# library(Rcpp)
+# library(dplyr)
+# library(rlang)
+# library(scales)
+# library(tibble)
 #library(ggcounty)
 #library(sp)
 #library(ggplot2)
@@ -9,27 +14,99 @@ library(choroplethrMaps)
 #library(mapproj)
 
 df<-read.csv("mapdata.csv")
+df$region <- df$FIPS
+data(continental_us_states)
 # get the US counties map (lower 48)
-us <- ggcounty.us()
+#us <- ggcounty.us()
 
 ui<-fluidPage(
+  includeMarkdown("include.md"),
   sliderInput(inputId = "year",
               label = "Choose a Year",
               value = 2005, min = 1999, max = 2010, step = 1, sep = ""),
-  plotOutput("yr"),
-  plotOutput("map")
+  selectInput(inputId = "state", label = "Choose a state to zoom in on:",
+              list (`states`=c("Alabama",
+                               "Arizona",
+                               "Arkansas",
+                               "California",
+                               "Colorado",
+                               "Connecticut",
+                               "Delaware",
+                               "Florida",
+                               "Georgia",
+                               "Idaho",
+                               "Illinois",
+                               "Indiana",
+                               "Iowa",
+                               "Kansas",
+                               "Kentucky",
+                               "Louisiana",
+                               "Maine",
+                               "Maryland",
+                               "Massachusetts",
+                               "Michigan",
+                               "Minnesota",
+                               "Mississippi",
+                               "Missouri",
+                               "Montana",
+                               "Nebraska",
+                               "Nevada",
+                               "New Hampshire",
+                               "New Jersey",
+                               "New Mexico",
+                               "New York",
+                               "North Carolina",
+                               "North Dakota",
+                               "Ohio",
+                               "Oklahoma",
+                               "Oregon",
+                               "Pennsylvania",
+                               "Rhode Island",
+                               "South Carolina",
+                               "South Dakota",
+                               "Tennessee",
+                               "Texas",
+                               "Utah",
+                               "Vermont",
+                               "Virginia",
+                               "Washington",
+                               "West Virginia",
+                               "Wisconsin",
+                               "Wyoming"))
+  ),
+  plotOutput("map"),
+  fluidRow(
+    column(6, align = "center", "Histogram of County TFP Price",
+           fluidRow(plotOutput("yr"))),
+    column(6, align = "center", textOutput("statemapyr"),
+           fluidRow(plotOutput("statemap"))
+    )
+  )  
+  
 )
 server<- function(input, output) {
   output$yr <-  renderPlot({
-    title <- "Histogram of County Thrifty Food Plan (TFP) Price"
+    title <- ""#Histogram of County TFP Price"
     hist(unlist(df[paste0("TFPYRM", input$year)]), breaks=20, main = title, 
-         xlab = "Price of Weekly TFP, Family of 4 (2010 Dollars)", xlim=c(146,250))
+         xlab = "Price of Weekly TFP, Family of 4, 2010 Dollars", xlim=c(146,250))
   })
   
   output$map <- renderPlot({
-    df$region <- df$FIPS
     df$value <- unlist(df[paste0("TFPYRM", input$year)])
-    county_choropleth(df)
+    county_choropleth(df, 
+            state_zoom = continental_us_states)
+  })
+  
+  output$statemapyr <- renderText({paste(input$state, input$year, sep =" ")})
+  
+  output$statemap <- renderPlot({
+    df$value <- unlist(df[paste0("TFPYRM", input$year)])
+    county_choropleth(df, 
+    legend     = "TFP Price",
+    #title = paste(input$state, input$year, sep=" "),
+    num_colors = 7,
+    state_zoom = tolower(input$state),
+    reference_map = FALSE)
   })
   # output$map <- renderPlot({
   #   df$brk <- cut(unlist(df[paste0("TFPYRM", input$year)]),
