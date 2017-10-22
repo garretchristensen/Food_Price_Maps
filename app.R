34dbbee5d3305d915b257679dad3731bf1cc6f92
Garret Christensen <garret@berkeley.edu>
2017-10-21 18:15:04 -0700

    switch to choroplethr, more hist bins

--- app.R
+++ app.R
@@ -1,10 +1,12 @@
 library(shiny)
-library(ggcounty)
-library(sp)
-library(ggplot2)
-library(maptools)
-library(rgeos)
-library(mapproj)
+library(choroplethr)
+library(choroplethrMaps)
+#library(ggcounty)
+#library(sp)
+#library(ggplot2)
+#library(maptools)
+#library(rgeos)
+#library(mapproj)
 
 df<-read.csv("mapdata.csv")
 # get the US counties map (lower 48)
@@ -19,26 +21,31 @@ ui<-fluidPage(
 )
 server<- function(input, output) {
   output$yr <-  renderPlot({
-    title <- "Histogram of County TFPs"
-    hist(unlist(df[paste0("TFPYRM", input$year)]), main = title,
-         xlab = "Dollar Price of Weekly TFP, Family of 4")
+    title <- "Histogram of County Thrifty Food Plan (TFP) Price"
+    hist(unlist(df[paste0("TFPYRM", input$year)]), breaks=20, main = title, 
+         xlab = "Price of Weekly TFP, Family of 4 (2010 Dollars)", xlim=c(146,250))
   })
   
   output$map <- renderPlot({
-    df$brk <- cut(unlist(df[paste0("TFPYRM", input$year)]),
-                  breaks=c(0, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250),
-                  labels=c("0-140", "-149", "-159", "-169", "-179", "-189",
-                           "-199", "-209", "-219", "-229", "-239", "-250"),
-                  include.lowest=TRUE)
-
-    # start the plot with our base map
-    gg <- us$g
-
-    #add a new geom with our population (choropleth)
-    gg <- gg + geom_map(data=df, map=us$map,
-                        aes(map_id=FIPS, fill=brk),
-                        color="white", size=0.25)  + scale_fill_brewer(palette = "Greens" )
-    gg
+    df$region <- df$FIPS
+    df$value <- unlist(df[paste0("TFPYRM", input$year)])
+    county_choropleth(df)
   })
+  # output$map <- renderPlot({
+  #   df$brk <- cut(unlist(df[paste0("TFPYRM", input$year)]),
+  #                 breaks=c(0, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250),
+  #                 labels=c("0-140", "-149", "-159", "-169", "-179", "-189",
+  #                          "-199", "-209", "-219", "-229", "-239", "-250"),
+  #                 include.lowest=TRUE)
+  # 
+  #   # start the plot with our base map
+  #   gg <- us$g
+  # 
+  #   #add a new geom with our population (choropleth)
+  #   gg <- gg + geom_map(data=df, map=us$map,
+  #                       aes(map_id=FIPS, fill=brk),
+  #                       color="white", size=0.25)  + scale_fill_brewer(palette = "Greens" )
+  #   gg
+  #})
 }
 shinyApp(ui = ui, server = server)
